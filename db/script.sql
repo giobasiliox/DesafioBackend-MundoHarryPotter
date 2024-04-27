3e80a12a193386c375e7c12fbce067b3a5431cbf
giobasiliox <giovana.basilio@aluno.senai.br>
2024-04-27 18:15:02 -0300

    fear: corrigir erros de digitação e adicionar novos bruxos e varinhas

--- db/script.sql
+++ db/script.sql
@@ -8,23 +8,19 @@ CREATE TABLE bruxo (
     patrono VARCHAR(100)
 );
 
+
 CREATE TABLE varinha (
     id SERIAL  PRIMARY KEY ,
     material VARCHAR(100) NOT NULL,
-    compimento DECIMAL(10,2) NOT NULL,
+    comprimento DECIMAL(10,2) NOT NULL,
     nucleo VARCHAR(100) NOT NULL,
     data_fabricacao DATE NOT NULL
 );
 
 "INSERT INTO bruxo (nome, idade, casahogwarts, habilidades, status_sangue, patrono) VALUES ('Harry Potter', 17, 'Grifinória', 'Voo', 'Mestiço', 'Cervo')"
 
-"nome": "Harry Potter",
-"idade": 17,
-"casahogwarts": "Grifinória",
-"habilidades": "Voo",
-"status_sangue": "Mestiço",
-"patrono": "Cervo"
 
+-- Bruxos
 "nome": "Hermione Granger",
 "idade": 17,
 "casahogwarts": "Grifinória",
@@ -38,6 +34,55 @@ CREATE TABLE varinha (
 "habilidades": "Xadrez",
 "status_sangue": "Puro",
 "patrono": "Jack Russel"
+
+"nome": "Draco Malfoy",
+"idade": 17,
+"casahogwarts": "Sonserina",
+"habilidades": "Poções",
+"status_sangue": "Puro",
+"patrono": "Serpente"
+
+"nome": "Luna Lovegood",
+"idade": 17,
+"casahogwarts": "Corvinal",
+"habilidades": "Adivinhação",
+"status_sangue": "Puro",
+"patrono": "Lebre"
+
+
+-- Varinhas
+"material": "Madeira de Teixo",
+"comprimento": 11.5,
+"nucleo": "Pena de Fênix",
+"data_fabricacao": "1991-07-31"
+
+"material": "Videira",
+"comprimento": 10.75,
+"nucleo": "Pelo de Unicórnio",
+"data_fabricacao": "1991-09-01"
+
+"material": "Salgueiro",
+"comprimento": 14,
+"nucleo": "Pelo de Rabo-Córneo Húngaro",
+"data_fabricacao": "1991-08-04"
+
+
+"material": "Espinheiro Negro",
+"comprimento": 10,
+"nucleo": "Pelo de Coração de Dragão",
+"data_fabricacao": "1991-06-24"
+
+"material": "Choupo",
+"comprimento": 12,
+"nucleo": "Cabelo de Veela",
+"data_fabricacao": "1991-08-05"
+
+      
+
+
+
+
+
 
 "INSERT INTO varinha (material, compimento, nucleo, data_fabricacao) VALUES ('Madeira de Teixo', 30.0, 'Pena de Fênix', '1991-07-31')"
 
